8ffa95378a77c2be69a2b462e50e1e187e49e5aa
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-03-30 12:49:37 -0300

    genera token en register y login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const db  = require('../database/models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     processRegister : (req,res) => {
@@ -9,8 +12,20 @@ module.exports = {
             pass : bcrypt.hashSync(pass.trim(),12)
         })
         .then(user => {
-            res.status(200).json({
-                msg : "usuario registrado con éxito"
+            const token = jwt.sign(
+                {
+                    id : user.id
+                },
+                process.env.TOP_SECRET,
+                {
+                    expiresIn : 60*2
+                }
+            )
+
+            return res.status(200).json({
+                auth : true,
+                msg : "registro exitoso",
+                token
             })
         })
         .catch(error => res.status(500).json(error))
@@ -18,6 +33,14 @@ module.exports = {
     },
     processLogin : (req,res) => {
         const {email, pass} = req.body
+
+        if(!email || !pass){
+            return res.status(401).json({
+                auth : false,
+                msg : 'faltan datos'
+            })
+        }
+
         db.User.findOne({
             where :{
                 email
@@ -25,11 +48,27 @@ module.exports = {
         })
         .then(user => {
             if(!user || !bcrypt.compareSync(pass.trim(),user.pass)){
-                return res.status(404).json({msg: 'credenciales inválidas'})
+                return res.status(404).json({
+                    auth : false,
+                    msg: 'credenciales inválidas'
+                })
             }
-            res.status(200).json({msg : 'Bienvenido!!'})
 
+            const token = jwt.sign(
+                {
+                    id : user.id
+                },
+                process.env.TOP_SECRET,
+                {
+                    expiresIn : 60*2
+                }
+            )
+            return res.status(200).json({
+                auth : true,
+                msg : "usuario logueado",
+                token
+            })
         })
-
+        .catch(error => res.status(500).json(error))
     }
 }
