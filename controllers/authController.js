6ce516f23308f36cccc93e9da02f2ff9902c3660
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-03-30 12:18:00 -0300

    agrega bcrypt , register y login completos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,12 @@
-const db  = require('../database/models')
+const db  = require('../database/models');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     processRegister : (req,res) => {
         const {email, pass} = req.body;
         db.User.create({
             email,
-            pass
+            pass : bcrypt.hashSync(pass.trim(),12)
         })
         .then(user => {
             res.status(200).json({
@@ -17,6 +18,18 @@ module.exports = {
     },
     processLogin : (req,res) => {
         const {email, pass} = req.body
+        db.User.findOne({
+            where :{
+                email
+            }
+        })
+        .then(user => {
+            if(!user || !bcrypt.compareSync(pass.trim(),user.pass)){
+                return res.status(404).json({msg: 'credenciales inválidas'})
+            }
+            res.status(200).json({msg : 'Bienvenido!!'})
+
+        })
 
     }
 }
